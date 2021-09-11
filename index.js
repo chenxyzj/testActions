287cab7681de179b0235806f6f82596ae25bf032
chenxyzj <chenxyzj@163.com>
2021-09-11 10:51:35 +0800

    增加app3路由

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ app.get("/app1",(req,res) =>
 app.get("/app2",(req,res) => 
  res.send(`APPID: ${APPID} app2's page.`));
 
+ app.get("/app3",(req,res) => 
+ res.send(`APPID: ${APPID} app3's page.`));
+
 app.listen(APPID,() => console.log(`${APPID} is listening on port ${APPID}...`));
